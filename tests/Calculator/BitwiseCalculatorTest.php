79f8cc55da6d62a0ab1e28138c09b8b9069f5e1a
Scott Culcheth <>
2018-05-13 22:29:57 +0000

    Added Bitwise tests

---
+++ tests/Calculator/BitwiseCalculatorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Calculator;
+
+use PHPUnit\Framework\TestCase;
+
+class BitwiseCalculatorTest extends TestCase
+{
+    protected $calculator;
+
+    public function setUp()
+    {
+        $this->calculator = new BitwiseCalculator();
+    }
+
+    public function testAnd()
+    {
+        $result = $this->calculator->bitAnd(15, 3);
+        $this->assertEquals(3, $result);
+    }
+
+    public function testOr()
+    {
+        $result = $this->calculator->bitOr(15, 3);
+        $this->assertEquals(15, $result);
+    }
+}
