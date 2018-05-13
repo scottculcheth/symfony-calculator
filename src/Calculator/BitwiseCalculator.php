f8222cbd191605563facebb6da6702859406f2f8
Scott Culcheth <>
2018-05-13 23:05:42 +0000

    Added type-hinting to cast to int on the way in to the calculation functionality

--- src/Calculator/BitwiseCalculator.php
+++ src/Calculator/BitwiseCalculator.php
@@ -3,12 +3,12 @@ namespace App\Calculator;
 
 class BitwiseCalculator implements BitwiseCalculatorInterface
 {
-    public function bitAnd($x, $y)
+    public function bitAnd(int $x, int $y)
     {
         return $x & $y;
     }
 
-    public function bitOr($x, $y)
+    public function bitOr(int $x, int $y)
     {
        return $x | $y;
     }
--- src/Calculator/BitwiseCalculatorInterface.php
+++ src/Calculator/BitwiseCalculatorInterface.php
@@ -3,6 +3,6 @@ namespace App\Calculator;
 
 interface BitwiseCalculatorInterface
 {
-    public function bitAnd($x, $y);
-    public function bitOr($x, $y);
+    public function bitAnd(int $x, int $y);
+    public function bitOr(int $x, int $y);
 }
